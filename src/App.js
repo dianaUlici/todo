ea32587a3f47669832fc4084adf721d58ebacc64
Diana Ulici <ulici_diana@yahoo.com>
2021-09-16 12:08:08 +0200

    to do list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DatePicker from 'react-datepicker';
+ 
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "This is a simple todo",
+      isDone: false,
+      deadline: Date().toLocaleString()
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List Trial Day Diana</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
+  );
+}
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div className="todo">
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+        {todo.text}{' '}
+        {todo.deadline ? new Date(todo.deadline).toLocaleString('en-GB') : ""}
+      </span>
+      <div>
+        <Button variant="outline-success" onClick={() =>
+          markTodo(index)}>✓
+        </Button> {' '}
+        <Button variant="outline-danger" onClick={() =>
+          removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+  );
+}
+
+function FormTodo ({addTodo}) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) 
+      {
+        alert("Item needs a text");
+        return;
+      }
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+    <Form.Group>
+      <Form.Label><b>Add Todo</b></Form.Label>
+      <Form.Control type ="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo text" />
+      <Form.Control type ="date" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo deadline" />
+    </Form.Group>
+    <Button variant="primary mb-3" type="submit">Submit</Button>
+    </Form>
   );
 }
 
